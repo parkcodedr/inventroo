b880b3a21e23ebb3b77284990a6f9f8325692ff6
parkcodedr <okwori32@gmail.com>
2022-03-15 00:53:35 +0100

    dashboard matrix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,26 +2,26 @@ const Dashboard = ()=>{
   return(
     <div className="content-body">
       <h2 className="font-weight-bold ml-3">Dashboard</h2>
-      <section className="row p-3 activity-wrapper mx-auto">
+      <section className="row p-2 activity-wrapper mx-auto">
         <div className="col-md-8">
           <h5 className="font-weight-bold">Sales Activity</h5>
           <div className="row">
-          <div className="col-md-2 activity-card">
+          <div className="col activity-card">
             <h4 className="font-weight-bold">0</h4>
             <p>QTY</p>
             <p>TO BE PACKED</p>
           </div>
-          <div className="col-md-2 activity-card">
+          <div className="col activity-card">
             <h4 className="font-weight-bold">0</h4>
             <p>QTY</p>
             <p>TO BE PACKED</p>
           </div>
-          <div className="col-md-2 activity-card">
+          <div className="col activity-card">
             <h4 className="font-weight-bold">0</h4>
             <p>QTY</p>
             <p>TO BE PACKED</p>
           </div>
-          <div className="col-md-2 activity-card">
+          <div className="col activity-card">
             <h4 className="font-weight-bold">0</h4>
             <p>QTY</p>
             <p>TO BE PACKED</p>
@@ -42,6 +42,55 @@ const Dashboard = ()=>{
 
       </section>
 
+      <div className="row mt-1">
+        <div className="col product-card border p-2 m-1">
+          <h4 className="font-weight-bold">Product Details</h4>
+          <hr/>
+          <section className="row">
+          <div className="product-stat col-md-7 p-2">
+          <div className="item d-flex justify-content-between">
+            <p>Low stock items</p>
+            <p>0</p>
+          </div>
+          <div className="item d-flex justify-content-between ">
+            <p>All group items</p>
+            <p>0</p>
+          </div>
+          <div className="item d-flex justify-content-between ">
+            <p>All items</p>
+            <p>0</p>
+          </div>
+          </div>
+          <div className="col-md-5">
+          <p className="text-center">Active Items</p>
+          <div class="progress">
+                <svg>
+                  <circle cx="38" cy="38" r="36"></circle>
+                </svg>
+                <div class="number">
+                  <p>0%</p>
+
+                </div>
+              </div>
+          </div>
+          </section>
+        
+        </div>
+        <div className="col product-card border m-1 p-2">
+        <div className="d-flex justify-content-between product-title">
+        <h4 className="font-weight-bold">Top Selling Items</h4>
+        <h4 className="font-weight-bold">This month</h4>
+         
+        </div>
+        <hr/>
+        <div className="d-flex justify-content-center align-items-center">
+          <p>No item were invoiced in this time frame</p>
+        </div>
+        </div>
+      </div>
+
+
+    
     </div>
   )
 }
