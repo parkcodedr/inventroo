9211048b84bc263217edd40add7886699d4d0b1a
ParkCodeDr <33763824+parkcodedr@users.noreply.github.com>
2022-07-01 11:07:04 +0100

    service flipcard
    service flip card completed

--- src/pages/home/Store.js
+++ src/pages/home/Store.js
@@ -1,4 +1,10 @@
+import { useRef } from 'react';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import {Link} from 'react-router-dom';
+import { hoverState } from 'components/features';
+
 const Store = ()=>{
+  const ref = useRef();
 return(
     <>
     <div className="row mx-auto">
@@ -14,33 +20,47 @@ return(
     </section>
     <div className="row mx-auto justify-content-center mt-3">
 
-    {/* <div className="col-md-4 m-1 flip">
-    <div class="flip-card-inner">
-    <div class="flip-card-front">
-    <img src="/app-assets/images/backgrounds/restaurant image.png" className="image-responsive" />
-    </div>
+    <section className="col-md-9">
+    <div className="row">
     
-    <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+    {hoverState.map(state=>(
+      
+      <div className="col-md-6 mb-1" key={state.id}>
+      <Flippy
+        flipOnHover={true} // default false
+        flipOnClick={true} // default false
+        flipDirection="horizontal" // horizontal or vertical
+        ref={ref} // to use toggle method like ref.curret.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+    >
+      <FrontSide style={{border:'none',padding:'0px'}}>
+      <img src={`/app-assets/images/backgrounds/${state.image}`} alt={state.title} className="image-responsive" />
+      </FrontSide>
+      <BackSide style={{ backgroundColor: '#62E1AA'}}>
+      <div className="text-center mb-1">
+      
+      <img src={`/app-assets/images/backgrounds/${state.icon}`} alt={state.title} className="flip-card-image" />
+      </div>
+        <h5 className="text-white text-center">{state.title}</h5>
+        <p className="hero-text text-center">{state.text}</p>
+        <div className="text-center mt-1">
+        <Link to={state.link} >
+        <button className="btn btn-main">Apply Now</button>
+        </Link>
+        </div>
+        
+        
+      </BackSide>
+    </Flippy>
+      
+      </div>
+      ))}
     </div>
-    </div>
+     
 
-    </div> */}
-
-<div className="col-md-4 m-1">
-    <img src="/app-assets/images/backgrounds/restaurant image.png" className="image-responsive"/>
-    </div>
-    <div className="col-md-4 m-1">
-    <img src="/app-assets/images/backgrounds/retail stores & supermarket.png" className="image-responsive"/>
-    </div>
-    <div className="col-md-4 m-1">
-    <img src="/app-assets/images/backgrounds/service businesses.png" className="image-responsive"/>
-    </div>
-    <div className="col-md-4 m-1">
-    <img src="/app-assets/images/backgrounds/fashion apparel.png" className="image-responsive" />
-    </div>
+    </section>
+    
     </div>
     </div>
     </>
